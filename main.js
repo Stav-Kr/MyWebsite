9da1cd1399babe3abec822677c29c5175571084b
Stavros Karathanasis <83426703+Stav-Kr@users.noreply.github.com>
2021-10-01 14:54:35 +0100

    add game

--- main.js
+++ main.js
@@ -17,3 +17,52 @@ function closeModal() {
     modal.style.display = 'none';
 }
 
+const rollBtn = document.querySelector('.btnClick');
+rollBtn.addEventListener('click', DiceRoll);
+
+
+function DiceRoll(roll1, roll2) {
+
+    var roll1 = Math.trunc(Math.random() * 6) + 1
+    var roll2 = Math.trunc(Math.random() * 6) + 1
+
+    document.querySelector(".dice_img_1").setAttribute("src",
+        "dice" + roll1 + ".png");
+
+    document.querySelector(".dice_img_2").setAttribute("src",
+        "dice" + roll2 + ".png");
+
+    if (roll1 === roll2) {
+        document.querySelector(".score").innerHTML = "Draw!";
+
+    } else if (roll1 < roll2) {
+        document.querySelector(".score").innerHTML = (" Morgan wins!");
+
+    } else {
+        document.querySelector(".score").innerHTML = (" You win!");
+
+        //document.querySelector("h4").innerHTML = score;
+    }
+
+    function scoreAdd(roll1, roll2) {
+        let score = 0;
+        if (roll1 > roll2) {
+            score++;
+            //document.querySelector("h4").innerHTML = score;
+        } else if (roll1 < roll2) {
+            score--;
+            // document.querySelector("h4").innerHTML = score;
+        } else {
+            score = score;
+            // document.querySelector("h4").innerHTML = score;
+        }
+
+
+
+    }
+
+
+
+
+
+}
